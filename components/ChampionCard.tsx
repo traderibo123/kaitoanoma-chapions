43262dc53a40ba00536451a252415d8f1f006df7
Traderibo123 <161652314+traderibo123@users.noreply.github.com>
2025-07-09 04:01:37 +0300

    Update ChampionCard.tsx

--- components/ChampionCard.tsx
+++ components/ChampionCard.tsx
@@ -18,34 +18,29 @@ export default function ChampionCard({ data }: { data: RankData }) {
         objectFit="cover"
         priority
       />
-      
+
       {/* Username */}
-      <div className="absolute top-[70px] w-full text-center text-[24px] font-bold text-white tracking-widest">
+      <div className="absolute top-[78px] w-full text-center text-[24px] font-extrabold text-white tracking-widest">
         {data.username.toUpperCase()}
       </div>
 
-      {/* Ranks */}
-      <div className="absolute left-[50px] top-[360px] text-white text-sm leading-6 font-semibold space-y-2">
-        <div className="flex gap-2">
-          <span className="w-[80px]">7 Days</span>
-          <span className="w-[50px]">:</span>
-          <span>{data["7d"].title}</span>
-        </div>
-        <div className="flex gap-2">
-          <span className="w-[80px]">30 Days</span>
-          <span className="w-[50px]">:</span>
-          <span>{data["30d"].title}</span>
-        </div>
-        <div className="flex gap-2">
-          <span className="w-[80px]">3 Months</span>
-          <span className="w-[50px]">:</span>
-          <span>{data["90d"].title}</span>
-        </div>
-        <div className="flex gap-2">
-          <span className="w-[80px]">6 Months</span>
-          <span className="w-[50px]">:</span>
-          <span>{data["180d"].title}</span>
-        </div>
+      {/* Ranks Table */}
+      <div className="absolute left-[45px] top-[355px] w-[310px] h-[140px] grid grid-rows-4 grid-cols-3 text-white text-[14px] font-medium items-center text-center leading-none">
+        <div className="col-span-1">7 Days</div>
+        <div className="col-span-1">:</div>
+        <div className="col-span-1">{data["7d"].title}</div>
+
+        <div className="col-span-1">30 Days</div>
+        <div className="col-span-1">:</div>
+        <div className="col-span-1">{data["30d"].title}</div>
+
+        <div className="col-span-1">3 Months</div>
+        <div className="col-span-1">:</div>
+        <div className="col-span-1">{data["90d"].title}</div>
+
+        <div className="col-span-1">6 Months</div>
+        <div className="col-span-1">:</div>
+        <div className="col-span-1">{data["180d"].title}</div>
       </div>
     </div>
   );
