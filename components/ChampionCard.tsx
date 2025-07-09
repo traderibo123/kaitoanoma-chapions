a894de57339bd255069b567673bb962766cef716
Traderibo123 <161652314+traderibo123@users.noreply.github.com>
2025-07-09 04:07:23 +0300

    Update ChampionCard.tsx

--- components/ChampionCard.tsx
+++ components/ChampionCard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 interface RankData {
   title: string;
+  rank: number | null;
 }
 
 interface ChampionCardProps {
@@ -15,10 +16,14 @@ interface ChampionCardProps {
   };
 }
 
+const formatRankTitle = (rank: number | null, title: string) => {
+  if (rank === null || rank === undefined) return title;
+  return `#${rank} ${title}`;
+};
+
 export default function ChampionCard({ data }: ChampionCardProps) {
   return (
     <div className="relative w-[400px] h-[600px]">
-      {/* Background Image */}
       <Image
         src="/card-template.png"
         alt="Card Background"
@@ -27,31 +32,36 @@ export default function ChampionCard({ data }: ChampionCardProps) {
         priority
       />
 
-      {/* Username */}
       <div className="absolute top-[95px] w-full text-center text-[24px] font-extrabold text-white tracking-wide">
         {data.username.toUpperCase()}
       </div>
 
-      {/* Ranks Table */}
       <div className="absolute left-[45px] top-[345px] w-[310px] h-[140px] grid grid-rows-4 grid-cols-3 text-white text-[14px] font-medium items-center text-center leading-none">
         <div className="col-span-1">7 Days</div>
         <div className="col-span-1">:</div>
-        <div className="col-span-1">{data["7d"].title}</div>
+        <div className="col-span-1">
+          {formatRankTitle(data["7d"].rank, data["7d"].title)}
+        </div>
 
         <div className="col-span-1">30 Days</div>
         <div className="col-span-1">:</div>
-        <div className="col-span-1">{data["30d"].title}</div>
+        <div className="col-span-1">
+          {formatRankTitle(data["30d"].rank, data["30d"].title)}
+        </div>
 
         <div className="col-span-1">3 Months</div>
         <div className="col-span-1">:</div>
-        <div className="col-span-1">{data["90d"].title}</div>
+        <div className="col-span-1">
+          {formatRankTitle(data["90d"].rank, data["90d"].title)}
+        </div>
 
         <div className="col-span-1">6 Months</div>
         <div className="col-span-1">:</div>
-        <div className="col-span-1">{data["180d"].title}</div>
+        <div className="col-span-1">
+          {formatRankTitle(data["180d"].rank, data["180d"].title)}
+        </div>
       </div>
 
-      {/* Footer */}
       <div className="absolute bottom-[45px] left-0 w-full text-center text-white text-[14px] font-semibold tracking-wide">
         CREATED BY TRADERIBO123
       </div>
