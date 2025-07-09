199da87f79945d150e755c1689677d2b1378a93b
Traderibo123 <161652314+traderibo123@users.noreply.github.com>
2025-07-09 04:04:51 +0300

    Update ChampionCard.tsx

--- components/ChampionCard.tsx
+++ components/ChampionCard.tsx
@@ -1,31 +1,39 @@
+// components/ChampionCard.tsx
 import Image from "next/image";
 
 interface RankData {
-  username: string;
-  "7d": { title: string };
-  "30d": { title: string };
-  "90d": { title: string };
-  "180d": { title: string };
+  title: string;
 }
 
-export default function ChampionCard({ data }: { data: RankData }) {
+interface ChampionCardProps {
+  data: {
+    username: string;
+    "7d": RankData;
+    "30d": RankData;
+    "90d": RankData;
+    "180d": RankData;
+  };
+}
+
+export default function ChampionCard({ data }: ChampionCardProps) {
   return (
     <div className="relative w-[400px] h-[600px]">
+      {/* Background Image */}
       <Image
         src="/card-template.png"
-        alt="Card Template"
+        alt="Card Background"
         layout="fill"
         objectFit="cover"
         priority
       />
 
       {/* Username */}
-      <div className="absolute top-[78px] w-full text-center text-[24px] font-extrabold text-white tracking-widest">
+      <div className="absolute top-[95px] w-full text-center text-[24px] font-extrabold text-white tracking-wide">
         {data.username.toUpperCase()}
       </div>
 
       {/* Ranks Table */}
-      <div className="absolute left-[45px] top-[355px] w-[310px] h-[140px] grid grid-rows-4 grid-cols-3 text-white text-[14px] font-medium items-center text-center leading-none">
+      <div className="absolute left-[45px] top-[345px] w-[310px] h-[140px] grid grid-rows-4 grid-cols-3 text-white text-[14px] font-medium items-center text-center leading-none">
         <div className="col-span-1">7 Days</div>
         <div className="col-span-1">:</div>
         <div className="col-span-1">{data["7d"].title}</div>
@@ -41,6 +49,11 @@ export default function ChampionCard({ data }: { data: RankData }) {
         <div className="col-span-1">6 Months</div>
         <div className="col-span-1">:</div>
         <div className="col-span-1">{data["180d"].title}</div>
+      </div>
+
+      {/* Footer */}
+      <div className="absolute bottom-[45px] left-0 w-full text-center text-white text-[14px] font-semibold tracking-wide">
+        CREATED BY TRADERIBO123
       </div>
     </div>
   );
